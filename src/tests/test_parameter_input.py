3163ae36e0cba6449e7457896e6e0a0c3fd0f330
Vinci Wu <36609544+vwwuu@users.noreply.github.com>
2026-02-22 15:05:45 -0500

    Added invalid input test

--- src/tests/test_parameter_input.py
+++ src/tests/test_parameter_input.py
@@ -23,3 +23,20 @@ class TestInput(TestCase):
         actual = prompt_input()
         expected = CrystalParameters(3.852, 3.852, 3.723, 90.0, 90.0, 90.0)
         self.assertEqual(actual, expected)
+
+    def test_invalid_input(self, mocked_input):
+        mocked_input.side_effect = [
+            '3.852', # a 
+            '3.852', # b
+            'bad input', # invalid c
+            '3.723', # valid c
+            '90.0', # alpha
+            '90.0', # beta
+            'bad gam', # invalid gamma
+            '90.0', # valid gamma
+            'bad recip', # invalid reciprocal space 
+            'y' # valid reciprocal space
+            ]
+        actual = prompt_input()
+        expected = CrystalParameters(3.852, 3.852, 3.723, 90.0, 90.0, 90.0, True)
+        self.assertEqual(actual, expected)
