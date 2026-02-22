5b40b618c0a8bdfe7d168432dcbd67ddddfd4b7d
Vinci Wu <36609544+vwwuu@users.noreply.github.com>
2026-02-22 11:52:05 -0500

    Defined skeleton reciprocal metric tensor equations
    Defined functions for F constant and unit cell volume (squared)

--- src/equations/equations.py
+++ src/equations/equations.py
@@ -3,24 +3,50 @@ from dataclasses import asdict
 import numpy as np
 from math import cos, sin, tan
 
-'''
-Equation for calculating the metric tensor
-Returns:
-  Matrix of metric tensor calculations
-'''
 def metric_tensor (c: CrystalParameters) -> np.array:
-  r1c1 = c.a * c.a
+  '''
+  Equation to calculate the metric tensor (g_triclinic)
+  Parameters:
+    CrystalParameters class
+  Returns:
+    Matrix of metric tensor calculations
+  '''
+  r1c1 = c.a**2
   r1c2 = c.a * c.b * cos(c.gamma)
   r1c3 = c.a * c.c * cos(c.beta)
 
   r2c1 = c.b * c.a * cos(c.gamma)
-  r2c2 = c.b * c.b
+  r2c2 = c.b**2
   r2c3 = c.b * c.c * cos(c.alpha)
 
   r3c1 = c.c * c.a * cos(c.beta)
   r3c2 = c.c * c.b * cos(c.alpha)
-  r3c3 = c * c
+  r3c3 = c**2
 
   return np.array([r1c1, r1c2, r1c3],
                   [r2c1, r2c2, r2c3],
                   [r3c1, r3c2, r3c3])
+
+def reciprocal_metric_tensor (c: CrystalParameters) -> np.array:
+  '''
+  Equation to calculate the reciprocal metric tensor (g*_triclinic)
+  Returns:
+    Matrix of reciprocal metric tensor
+  '''
+  mt_matrix = np.array()
+  return mt_matrix
+
+def f_constant (alpha: float, beta: float, gamma: float) -> float:
+  return (cos(alpha) * cos(beta)) - cos(gamma)
+
+def unit_cell_volume_sq (c: CrystalParameters) -> float:
+  '''
+  Equation to calculate the squared volume of a unit cell
+  '''
+  a = (c.a**2) * (c.b**2) * (c.c**2)
+  b = (1 
+       - cos(c.alpha)**2 
+       - cos(c.beta)**2 
+       - cos(c.gamma)**2
+       + 2*cos(c.alpha) * cos(c.beta) * cos(c.gamma))
+  return a * b
