1646e5614e623958b5fe9ddb9a25548fc221a737
Vinci Wu <36609544+vwwuu@users.noreply.github.com>
2026-02-22 13:27:43 -0500

    Created test for reciprocal and replaced assertTrue with np.testing.assert_allclose
    Allows for decimal tolerance

--- src/tests/test_equations.py
+++ src/tests/test_equations.py
@@ -7,10 +7,31 @@ class TestEqs(unittest.TestCase):
   test_crystal = CrystalParameters(3.852, 3.852, 3.723, 90.0, 90.0, 90.0)
 
   def test_metric_tensor(self):
+    '''
+    Test for metric tensor function
+    '''
     actual_mt = eq.metric_tensor(self.test_crystal)
     expected_mt = np.array([
-      [14.837904, 0.0, 0.0],
-      [0.0, 14.837904, 0.0],
-      [0.0, 0.0, 13.860729]
+      [14.8379, 0.0, 0.0],
+      [0.0, 14.8379, 0.0],
+      [0.0, 0.0, 13.8607]
     ])
-    self.assertTrue((actual_mt==expected_mt).all())
+    np.testing.assert_allclose(actual_mt, 
+                               expected_mt, 
+                               rtol=1e-5,
+                               err_msg='compare reciprocal metric tensor')
+
+  def test_reciprocal_metric_tensor(self):
+    '''
+    Test for metric tensor function
+    '''
+    actual_mt = eq.reciprocal_metric_tensor(self.test_crystal)
+    expected_mt = np.array([
+      [0.067395, 0.0, 0.0],
+      [0.0, 0.067395, 0.0],
+      [0.0, 0.0, 0.0721463]
+    ])
+    np.testing.assert_allclose(actual_mt, 
+                               expected_mt, 
+                               rtol=1e-5,
+                               err_msg='compare reciprocal metric tensor')
