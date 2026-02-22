1a416507b8a227e8008984b1d73a0b0f47f7398c
Vinci Wu <36609544+vwwuu@users.noreply.github.com>
2026-02-22 14:49:49 -0500

    Added test for valid input
    mock patch for entire testing class, from only method level

--- src/tests/test_parameter_input.py
+++ src/tests/test_parameter_input.py
@@ -1,10 +1,25 @@
 from unittest import mock
 from unittest import TestCase
 from processing.parameter_handler import prompt_input
+from models.crystal_parameters import CrystalParameters
 
+@mock.patch('processing.parameter_handler.input', create=True)
 class TestInput(TestCase):
-    @mock.patch('processing.parameter_handler.input', create=True)
     def test_exit(self, mocked_input):
         mocked_input.side_effect = ['3.287', 'Exit']
         actual = prompt_input()
         self.assertIsNone(actual)
+
+    def test_valid_input(self, mocked_input):
+        mocked_input.side_effect = [
+            '3.852', # a 
+            '3.852', # b
+            '3.723', # c
+            '90.0', # alpha
+            '90.0', # beta
+            '90.0', # gamma
+            'n'
+            ]
+        actual = prompt_input()
+        expected = CrystalParameters(3.852, 3.852, 3.723, 90.0, 90.0, 90.0)
+        self.assertEqual(actual, expected)
