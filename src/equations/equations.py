1fa66c0416d5fd97f5de26633764f7377d1ce0e2
Vinci Wu <36609544+vwwuu@users.noreply.github.com>
2026-02-22 00:14:43 -0500

    Set up placeholders for metric_tensor
    Imported asdict and math trig functions

--- src/equations/equations.py
+++ src/equations/equations.py
@@ -1,5 +1,10 @@
 from models.crystal_parameters import CrystalParameters
+from dataclasses import asdict
 import numpy as np
+from math import cos, sin, tan
 
 def metric_tensor (crystal: CrystalParameters) -> np.array:
-  return np.array()
+  params = asdict(crystal)
+  return np.array([0, 0, 0],
+                  [0, 0, 0],
+                  [0, 0, 0])
