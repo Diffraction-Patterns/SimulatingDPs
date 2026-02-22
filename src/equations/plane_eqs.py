faa81a66dd8e52fd3501cb2d73db89e2edf8cf54
Vinci Wu <36609544+vwwuu@users.noreply.github.com>
2026-02-22 16:54:49 -0500

    Defined planes identification function

--- src/equations/plane_eqs.py
+++ src/equations/plane_eqs.py
@@ -2,4 +2,11 @@ import sympy as sym
 from models.zone_axis import ZoneAxis
 
 def planes_identification (zad: ZoneAxis):
-  return None
+  '''
+  Computes the Zone axis equation satisfaction:
+    hx + ky +Lz = 0
+  '''
+  h, k, L = sym.symbols('h k L')
+  x, y, z = zad.x, zad.y, zad.z
+  expr = h*x + k*y + L*z
+  return expr
