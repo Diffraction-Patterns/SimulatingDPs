e518994a9247d1b569becbb8843d6e248411018c
Vinci Wu <36609544+vwwuu@users.noreply.github.com>
2026-02-21 23:33:53 -0500

    Added exit option and renamed class

--- src/processing/parameter_handler.py
+++ src/processing/parameter_handler.py
@@ -1,4 +1,4 @@
-from CrysParams import CrysParams
+from processing.crystal_parameters import CrystalParameters
 
 '''
 Handles user inputs for parameters.
@@ -9,15 +9,15 @@ a, b, c, alpha, beta, and gamma
 Return: 
   CrysParams: An object populated with inputted parameters
 '''
-def prompt_input() -> CrysParams:
+def prompt_input() -> CrystalParameters:
   params = ['a', 'b', 'c', 'alpha', 'beta', 'gamma']
 
   while True:
     value_inputs = {}
+    print('Type "exit" to quit') 
     for p in params:
       while True:
         print(f'Enter value for [{p}]')
-        print('Type "exit" to quit') 
         raw_input = input().strip()
 
         if (raw_input.lower() == 'exit'):
@@ -28,9 +28,9 @@ def prompt_input() -> CrysParams:
           value_inputs[p] = float(raw_input)
           break
         except ValueError:
-          print('Please enter a valid number.')
+          print('Please enter a valid number.\n')
 
     try:
-      return CrysParams(**value_inputs)
+      return CrystalParameters(**value_inputs)
     except ValueError as e:
       print(f'Error with parameter: {e}')
