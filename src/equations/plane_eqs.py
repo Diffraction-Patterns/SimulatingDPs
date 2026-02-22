5573f55b9adc5edc1f7a1985226197a7d231ca2a
Vinci Wu <36609544+vwwuu@users.noreply.github.com>
2026-02-22 17:20:57 -0500

    Defined the zones axis planes identification

--- src/equations/plane_eqs.py
+++ src/equations/plane_eqs.py
@@ -1,12 +1,20 @@
-import sympy as sym
 from models.zone_axis import ZoneAxis
 
-def planes_identification (zad: ZoneAxis):
+def planes_identification(zad: ZoneAxis, maxRange: int = 10) -> list[tuple[int, int, int]]:
   '''
-  Computes the Zone axis equation satisfaction:
+  Computes the zone axis planes using the equation:
     hx + ky +Lz = 0
+  Return:
+    List of tuples representing points
   '''
-  h, k, L = sym.symbols('h k L')
   x, y, z = zad.x, zad.y, zad.z
-  expr = h*x + k*y + L*z
-  return expr
+  plane_directions = []
+
+  for h in range(-maxRange, maxRange+1):
+    for k in range(-maxRange, maxRange+1):
+      for l in range(-maxRange, maxRange+1):
+        if (h, k, l) == (0, 0, 0):
+          continue
+        if h*x + k*y + l*z == 0:
+          plane_directions.append((h, k, l))
+  return plane_directions
