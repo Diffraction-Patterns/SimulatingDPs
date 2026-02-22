4976599faec46008e5531a9f3fe329ff2b31398d
Vinci Wu <36609544+vwwuu@users.noreply.github.com>
2026-02-21 21:01:09 -0500

    Example main from Python templates
    Example piurposes

--- src/simulation/sim.py
+++ src/simulation/sim.py
@@ -1,2 +1,14 @@
-def main():
-  print('hello example')
+import shlex
+import sys
+
+def echo(phrase: str) -> None:
+   print(phrase)
+
+def main() -> int:
+    """Echo the input arguments to standard output"""
+    phrase = shlex.join(sys.argv)
+    echo(phrase)
+    return 0
+
+if __name__ == '__main__':
+    sys.exit(main())
