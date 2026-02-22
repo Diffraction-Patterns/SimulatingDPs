4e91f0195fd4b58815f0a0c48f0247db0ab55a29
Vinci Wu <36609544+vwwuu@users.noreply.github.com>
2026-02-22 15:56:39 -0500

    Rename test file to be distinct

--- src/tests/test_crystal_input.py
+++ src/tests/test_crystal_input.py
@@ -1,13 +1,13 @@
 from unittest import mock
 from unittest import TestCase
-from processing.parameter_handler import prompt_crystal_input
+from processing.parameter_handler import prompt_crystal
 from models.crystal_parameters import CrystalParameters
 
 @mock.patch('processing.parameter_handler.input', create=True)
-class TestInput(TestCase):
+class TestCrystalInput(TestCase):
     def test_exit(self, mocked_input):
         mocked_input.side_effect = ['3.287', 'Exit']
-        actual = prompt_crystal_input()
+        actual = prompt_crystal()
         self.assertIsNone(actual)
 
     def test_valid_input(self, mocked_input):
@@ -20,7 +20,7 @@ class TestInput(TestCase):
             '90.0', # gamma
             'n'
             ]
-        actual = prompt_crystal_input()
+        actual = prompt_crystal()
         expected = CrystalParameters(3.852, 3.852, 3.723, 90.0, 90.0, 90.0)
         self.assertEqual(actual, expected)
 
@@ -37,6 +37,6 @@ class TestInput(TestCase):
             'bad recip', # invalid reciprocal space 
             'y' # valid reciprocal space
             ]
-        actual = prompt_crystal_input()
+        actual = prompt_crystal()
         expected = CrystalParameters(3.852, 3.852, 3.723, 90.0, 90.0, 90.0, True)
         self.assertEqual(actual, expected)
