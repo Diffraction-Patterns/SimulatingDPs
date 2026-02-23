ef385735271ebcbd1ff20cce3011cb7fec27d0a5
Vinci Wu <36609544+vwwuu@users.noreply.github.com>
2026-02-22 21:30:09 -0500

    Created test for computing the plane vector magnitude

--- src/tests/test_plane_eqs.py
+++ src/tests/test_plane_eqs.py
@@ -1,13 +1,24 @@
 from unittest import TestCase
+import numpy as np
 import equations.plane_eqs as eq
 from models.zone_axis import ZoneAxis
+from models.lattice import Lattice
 
 class TestPlaneEqs(TestCase):
-  test_direction = ZoneAxis(1, 1, 0)
-
   def test_planes_ident(self):
     '''
     Test for planes identification
     '''
-    planes_range = eq.planes_identification(self.test_direction)
+    test_direction = ZoneAxis(1, 1, 0)
+    planes_range = eq.planes_identification(test_direction)
     self.assertIsNotNone(planes_range)
+
+  def test_planes_length(self):
+    '''
+    Test for planes length calculation
+    '''
+    test_plane = (0, 0, 1)
+    test_lattice = Lattice(3.852, 3.852, 3.723, 90.0, 90.0, 90.0)
+    actual = eq.plane_vector_length(test_plane, eq.reciprocal_metric_tensor(test_lattice))
+    expected = 0.268601
+    np.testing.assert_allclose(actual, expected, rtol=1e-5)
