7d20adc3b026ba372bc0bd49786ea1b2fe00ca48
Vinci Wu <36609544+vwwuu@users.noreply.github.com>
2026-02-22 21:15:45 -0500

    Defined function for vector magnitude calculation

--- src/equations/plane_eqs.py
+++ src/equations/plane_eqs.py
@@ -1,4 +1,6 @@
+import numpy as np
 from models.zone_axis import ZoneAxis
+from equations.lattice_eqs import reciprocal_metric_tensor
 
 def planes_identification(zad: ZoneAxis, maxRange: int = 10) -> list[tuple[int, int, int]]:
   '''
@@ -18,3 +20,15 @@ def planes_identification(zad: ZoneAxis, maxRange: int = 10) -> list[tuple[int, 
         if h*x + k*y + l*z == 0:
           plane_directions.append((h, k, l))
   return plane_directions
+
+def plane_vector_length(hkl: tuple[int, int, int], g_star: np.array) -> float:
+  '''
+  Calculates the magnitude of the given plane vector
+  Parameters:
+    hkl: tuple[int, int, int] - represents a vector of the plane (h, k, L)
+    g_star: np.array - matrix from calculating the reciprocal metric tensor
+  Return:
+    length of the given vector
+  '''
+  plane = np.asarray(hkl, dtype=float)
+  return float(np.sqrt(plane @ g_star @ plane)) # @ represents dot product (Python 3.5+)
