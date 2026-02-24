c9db7a425e32234a7799a6eba6ecbe6f3919e07e
Vinci Wu <36609544+vwwuu@users.noreply.github.com>
2026-02-23 23:08:08 -0500

    Removed needless distinction for smallest planes
    Removed for now, may need to reinclude
    
    Adjusted sizing

--- src/plot/dp_plotting.py
+++ src/plot/dp_plotting.py
@@ -11,23 +11,22 @@ def diffraction_pattern_plot (lattice: Lattice, zone_axis: ZoneAxis):
   planes = p_eqs.planes_identification(zone_axis)
   smallest_1, smallest_2 = p_eqs.two_shortest_planes(planes, lattice)
   
-  planes_filtered = np.stack([
-     p for p in planes
-     if not (np.array_equal(p, smallest_1)) or (np.array_equal(p, smallest_2))
-  ])
-  smallest_planes = np.stack([smallest_1, smallest_2])
+  planes_filtered = np.stack(
+    [smallest_1, smallest_2]
+    + [p for p in planes
+     if not (np.array_equal(p, smallest_1)) or (np.array_equal(p, smallest_2))]
+     )
 
   # plot planes (3D scatter)
   fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
   
-  ax.scatter(0, 0, 0, color='black', s=30)
+  ax.scatter(0, 0, 0, color='black', s=5)
+  ax.text(0, 0, 0, '000', fontsize=5)
 
-  for h, k, l in smallest_planes:
-    ax.scatter([h], [k], [l], color='black', s=30)
-    ax.text(h, k, l, f'({h}, {k}, {l})', fontsize=10)
-  
   for h, k, l in planes_filtered:
-     ax.scatter([h], [k], [l], color='black', s=30)
+    ax.scatter([h], [k], [l], color='black', s=5)
+    ax.text(h, k, l, f'{h}{k}{l}', fontsize=5)
+
 
   ax.set_axis_off()
   ax.view_init(elev=0, azim=0)
