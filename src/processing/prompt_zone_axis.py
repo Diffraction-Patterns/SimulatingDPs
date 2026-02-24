a428c216be32f9f86cd596430950a59cef42cf7e
Vinci Wu <36609544+vwwuu@users.noreply.github.com>
2026-02-23 20:52:49 -0500

    Update prompt to handle commans, spaces, and negatives

--- src/processing/prompt_zone_axis.py
+++ src/processing/prompt_zone_axis.py
@@ -13,21 +13,33 @@ def prompt_zone_axis_direction() -> ZoneAxis:
   params = ['x', 'y', 'z']
   while True:
     value_inputs = {}
-  
-    for idx, p in enumerate(params, start=1):
-      while True:
-        raw_input = input(f'{idx}. Enter value for [{p}]: ').strip()
+    print('\nEnter the zone axis direction as 3 integers (i.e. 1 2 3 for [1 2 3]: ')
+    raw_input = input().strip()
 
-        if raw_input.lower() == 'exit':
-          print("Exiting parameter input...")
-          return None
-        
-        try:
-          value_inputs[p] = int(raw_input)
-          break
-        except ValueError:
-          print('\nPlease enter a valid integer number.')
+    if raw_input.lower() == 'exit':
+      print('Exiting parameter input...')
+      return None
 
+    # account for valid inputs without space or commas (i.e. )    
+    if raw_input.isdigit() and len(raw_input) == 3:
+      direction = [int(d) for d in raw_input]
+    else:
+      # account for valid inputs separated by comma (,) or space ( )
+      input_d = raw_input.replace(',', ' ').split()
+
+      if len(input_d) != 3:
+         print('Invalid input. Please try again.')
+         continue
+      
+      try:
+          direction = [int(d) for d in input_d]
+      except ValueError:
+           print('\nPlease enter a valid integer number.')
+           continue
+
+    for idx, p in enumerate(params):
+      value_inputs[p] = direction[idx]
+    
     try:
       return ZoneAxis(**value_inputs)
     except ValueError as e:
