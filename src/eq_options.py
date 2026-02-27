adba5c4c16360abcc63dacb3c338b0b60909c8d6
Vinci Wu <36609544+vwwuu@users.noreply.github.com>
2026-02-26 22:23:53 -0500

    Added options map

---
+++ src/eq_options.py
@@ -0,0 +1,20 @@
+options = {
+  1 : {
+    "label": "Lattice",
+    "children": {
+      'a': 'f_constant',
+      'b': 'unit_cell_volume_sq',
+      'c': 'metric_tensor',
+      'd': 'reciprocal_metric_tensor'
+    }
+  },
+  2 : {
+    "label": 'Zone Axis Direction',
+    "children": {
+      'a': 'planes_identification',
+      'b': 'plane_vector_magnitude',
+      'c': 'two_shortest_planes',
+      'd': 'plane_angles'
+    }
+  }
+}
