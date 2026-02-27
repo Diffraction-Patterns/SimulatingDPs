4a293d0a10ac2d74faed05066ee51fe7e98fc8f7
Vinci Wu <36609544+vwwuu@users.noreply.github.com>
2026-02-26 21:41:22 -0500

    Defined validate c option function and clear prompt_option definition

--- src/processing/prompt_option.py
+++ src/processing/prompt_option.py
@@ -16,55 +16,18 @@ def validate_c_option(option: str) -> bool:
   Returns:
     true if character
   '''
+  return len(option) == 1 and str.isalpha(option)
   
-def prompt_lattice() -> Lattice:
-  '''
-  Handles user inputs for crystal parameters.
-
-  Prompts user for six floating point number parameter values:
-  a, b, c, alpha, beta, and gamma
-
-  Return: 
-    Lattice An object populated with inputted parameters
-  '''
-  params = ['a', 'b', 'c', 'alpha', 'beta', 'gamma']
-  rp_space_key = 'reciprocal_space'
-  print('Type "exit" to quit') 
-
+def prompt_option():
   while True:
-    value_inputs = {}
-  
-    for idx, p in enumerate(params, start=1):
-      while True:
-        raw_input = input(f'{idx}. Enter value for [{p}]: ').strip()
+    #raw_input = input(f'{idx}. Enter value for [{p}]: ').strip()
 
-        if raw_input.lower() == 'exit':
-          print("Exiting parameter input...")
-          return None
+        # if raw_input.lower() == 'exit':
+        #   print("Exiting parameter input...")
+        #   return None
         
-        try:
-          value_inputs[p] = float(raw_input)
-          break
-        except ValueError:
-          print('Please enter a valid number.\n')
-
-    ''' (NOT IN USE. may be used for future purposes)
-    while True:
-      raw_input = input('Reciprocal space [y/n]: ').strip().lower() 
-
-      if raw_input == 'exit':
-        print("Exiting parameter input...")
-        return None
-      
-      if raw_input in ('y', 'n'):
-        if raw_input == 'y':
-          print('Set reciprocal space...')
-          value_inputs[rp_space_key] = True
-        break
-      else:
-        print('Please enter only [y]es or [n]o.')
-    '''
-    try:
-      return Lattice(**value_inputs)
-    except ValueError as e:
-      print(f'Error with parameter: {e}')
+        # try:
+        #   value_inputs[p] = float(raw_input)
+        #   break
+        # except ValueError:
+        #   print('Please enter a valid number.\n')
