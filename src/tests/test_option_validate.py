1422add02c50e7491e46fa7f527c31a70b98bd7a
Vinci Wu <36609544+vwwuu@users.noreply.github.com>
2026-02-26 21:57:45 -0500

    Added additional test

--- src/tests/test_option_validate.py
+++ src/tests/test_option_validate.py
@@ -6,16 +6,20 @@ class TestOptions(TestCase):
     bad_op1 = '11'
     bad_op2 = 'a'
     bad_op3 = '5'
+    bad_op4 = '-2'
     op = '1'
     self.assertFalse(po.validate_p_option(bad_op1, 2))
     self.assertFalse(po.validate_p_option(bad_op2, 2))
     self.assertFalse(po.validate_p_option(bad_op3, 2))
+    self.assertFalse(po.validate_p_option(bad_op4, 2))
     self.assertTrue(po.validate_p_option(op, 2))
 
   def test_validate_child(self):
     bad_op1 = '1'
     bad_op2 = 'aa'
+    bad_op3 = '-'
     op = 'a'
     self.assertFalse(po.validate_c_option(bad_op1))
     self.assertFalse(po.validate_c_option(bad_op2))
+    self.assertFalse(po.validate_c_option(bad_op3))
     self.assertTrue(po.validate_c_option(op))
