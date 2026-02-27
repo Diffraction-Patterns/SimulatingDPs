d737dcd8b96827b7c0c81a03e45f29a920fbac17
Vinci Wu <36609544+vwwuu@users.noreply.github.com>
2026-02-26 23:11:19 -0500

    Correct test and added test for options

--- src/tests/test_option_input.py
+++ src/tests/test_option_input.py
@@ -4,14 +4,28 @@ from processing.prompt_option import prompt_options
 
 @mock.patch('processing.prompt_option.input', create=True)
 class TestOptionInput(TestCase):
-    def test_lattice(self, mocked_input):
-        mocked_input.side_effect = [
-            '3',
-            '1',
-            'b',
-            ]
-        actual1, actual2 = prompt_options()
-        print(actual1)
-        expected1, expected2 = 'Lattice', 'unit_cell_volume_sq'
-        self.assertEqual(actual1, expected1)
-        self.assertEqual(actual2, expected2)
+  def test_lattice_option(self, mocked_input):
+      mocked_input.side_effect = [
+          '3',
+          '1',
+          'b',
+          ]
+      actual1, actual2 = prompt_options()
+      print(actual1)
+      expected1, expected2 = 1, 'b'
+      self.assertEqual(actual1, expected1)
+      self.assertEqual(actual2, expected2)
+
+  def test_zone_axis_option(self, mocked_input):
+      mocked_input.side_effect = [
+          '3',
+          '2',
+          '6',
+          'u',
+          'd'
+          ]
+      actual1, actual2 = prompt_options()
+      print(actual1)
+      expected1, expected2 = 2, 'd'
+      self.assertEqual(actual1, expected1)
+      self.assertEqual(actual2, expected2)
