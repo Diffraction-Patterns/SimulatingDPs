ba0ddb2263199da12cca6d0b1c78394da3172f6e
Vinci Wu <36609544+vwwuu@users.noreply.github.com>
2026-02-27 20:24:13 -0500

    Renamed package to more accurate correspond to shift

--- src/diffraction_patterns/main.py
+++ src/diffraction_patterns/main.py
@@ -1,7 +1,10 @@
 import shlex
 import sys
+from eq_router import parameters
 from processing.prompt_lattice import prompt_lattice
 from processing.prompt_zone_axis import prompt_zone_axis_direction
+import equations.plane_eqs as eq
+import plot.dp_plotting as spl
 
 def simTitle() -> None:
     print('|====== Diffraction Patterns Simulation ======|')
@@ -10,15 +13,13 @@ def main() -> int:
     simTitle()
     try:
       print('\n| === Lattice Parameters ===|')
-      crystal_params = prompt_lattice()
+      parameters["lattice"] = prompt_lattice()
 
       print('\n| === Zone Axis Direction ===|')
-      zone_axis_direction = prompt_zone_axis_direction()
-      
-      if crystal_params != None:
-        print(crystal_params)
-      if zone_axis_direction != None:
-         print(zone_axis_direction)
+      parameters["zone_axis_direction"] = prompt_zone_axis_direction()
+
+      print(parameters["lattice"])
+      print(parameters["zone_axis_direction"])
     except KeyboardInterrupt:
        print ('\nExiting...')
        sys.exit(0)
