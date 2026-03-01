0ed804f3137d243c2f5341a7706dfa2aa2abd7d1
Vinci Wu <36609544+vwwuu@users.noreply.github.com>
2026-02-28 23:05:54 -0500

    Defined helper function to run f_constant equation

--- src/eq_selection/eq_selector.py
+++ src/eq_selection/eq_selector.py
@@ -1,19 +1,37 @@
 import equations.lattice_eqs as l_eq
 import equations.plane_eqs as p_eq
+from eq_options import options
+from params import parameters
 from models.lattice import Lattice
 from models.zone_axis import ZoneAxis
+from processing import *
 
+def execute_f_constant ():
+  choice = input('New alpha, beta, and gamma? [y/n]: ').strip()
+  if exit_prompt(choice):
+    return None
+  elif choice.lower() == 'y':
+    a, b, c = prompt_abc()
+    return l_eq.f_constant(a, b, c)
+  elif choice.lower() == 'n':
+    a, b, c = parameters['lattice'].a, parameters['lattice'].b, parameters['lattice'].c
+    return l_eq.f_constant(a, b, c)
+  else:
+    print('Invalid option.')
+    return None
 
-def equation_selection(p_option: str, c_option: str, lattice: Lattice, zone_axis: ZoneAxis, maxRange: int = 5):
+def equation_selection(p_option: int, c_option: str):
   '''
   Selects the appropriate equation based on p and c options
   Parameters:
-    p_option: str - parent option
+    p_option: int - parent option
     c_option: str - child option
     lattice - Lattice
     zone_axis - zone axis direction
   Returns:
-    equation output
+    evaluated equation results
   '''
-  p_op = validate_p_option(p_option)
-  cc_op = validate_c_option(c_option) 
+  selection = options[p_option]["children"][c_option]
+
+  if selection == "f_constant":
+    return execute_f_constant()
